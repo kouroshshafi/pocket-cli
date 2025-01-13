42226845c654427cd5bd5c5b87c6bf76746e2327
kourosh shafi <kouroshshafi@users.noreply.github.com>
2025-01-13 10:33:23 -0800

    Update pocket_comm.py

--- pocket_comm.py
+++ pocket_comm.py
@@ -1,259 +1,319 @@
-from pathlib import Path
-from pprint import pformat
-import json
-import requests
+from datetime import datetime
+from pocket_comm import PocketItem, Pocket, clean_pdf_filename
+import readline
+import shutil
 import subprocess as sp
-import sys
-import itertools
+from urllib.parse import urlparse
 from starter_program import OPEN_COMMAND
-from starter_program import PERCOL_COMMAND
-import time
-
-class PocketItem:
-    sort_idx: int
-    item_id: str
-    domain_name: str
-    given_title: str
-    given_url: str
-    time_added: int
-    time_to_read: int
-    word_count: int
-
-    WORDS_PER_MINUTE = 214
-
-    def __init__(
-        self,
-        item_id: str,
-        domain_name: str,
-        given_title: str,
-        given_url: str,
-        time_added: str,
-        time_to_read: str,
-        word_count: str,
-    ):
-        self.sort_idx = 0
-        self.item_id = item_id
-        self.domain_name = domain_name
-        self.given_title = given_title
-        self.given_url = given_url
-        self.time_added = int(time_added) if time_added.isdigit() else int(time.time())
-        self.time_to_read = self.get_time_to_read(int(word_count))
-        self.word_count = int(word_count)
-
-    @staticmethod
-    def get_time_to_read(word_count: int):
-        return int(round(word_count / PocketItem.WORDS_PER_MINUTE))
-
-    def get_grouped_time_to_read(self):
-        for limit in itertools.chain((1, 2), range(5, 1000, 5)):
-            if self.time_to_read <= limit:
-                return limit
+from starter_program import PERCOL_COMMAND, SAVE_PDF_DIR
+from tabulate import tabulate
+from collections import defaultdict
 
 
-class Pocket:
-    POCKET_ACCESS_TOKEN_FILE = Path.home() / ".pocket"
-    # you get this from Pocket after creating a new app
-    CONSUMER_KEY = "104395-a466587cb5dfff975b68a80"
-    REDIRECT_URI = "http://example.com/"
+def get_trimmed(s: str, max_len: int) -> str:
+    if len(s) > max_len:
+        return s[:max_len]
+    return s
 
-    def __init__(self):
-        self.access_token = None
-        self.username = None
-        self.items = dict()
-        self.batched_actions = []
 
-    def authenticate(self):
-        self.access_token, self.username = self.get_access_token_and_username()
+class PocketPrompt:
+    valid_commands = {
+        "v": "[v]iew <index>",
+        "d": "[d]elete <index>",
+        "da": "[d]elete [a]ll",
+        ".": "[.] domains",
+        "vd": "[vd] <index>",
+        "u": "[u]pdate",
+        "l": "by [l]ength",
+        "f": "[f]ilter <keyword>",
+        "s": "[s]ort",
+        "t": "[t]ag",
+        "q": "[q]uit",
+        "p": "[p]rint",
+    }
 
-    def send_request(self, pocket_uri: str, data: dict) -> dict:
-        headers = {"X-Accept": "application/json"}
-        r = requests.post(
-            f"https://getpocket.com/{pocket_uri}",
-            headers=headers,
-            json=data,
-        )
-        if r.status_code != 200:
-            print(
-                f"Request failed with status code {r.status_code}",
-                file=sys.stderr,
-            )
-            raise RuntimeError(pformat(dict(r.headers)))
-        return r.json()
+    def __init__(self, pocket: Pocket):
+        self.pocket: Pocket = pocket
+        self.items: dict[str, PocketItem] = {}
+        self.sort_by = 1
 
-    def send_batched_requests(self) -> None:
-        batched_send_requests = {
-            "consumer_key": Pocket.CONSUMER_KEY,
-            "access_token": self.access_token,
-            "actions": self.batched_actions,
-        }
-        r = requests.post(
-            "https://getpocket.com/v3/send",
-            headers={
-                "Content-Type": "application/json; charset=UTF-8",
-                "X-Accept": "application/json",
-            },
-            json=batched_send_requests,
-        )
-        if r.status_code != 200:
-            print(
-                f"Request failed with status code {r.status_code}",
-                file=sys.stderr,
-            )
-            raise RuntimeError(pformat(dict(r.headers)))
-        self.batched_actions.clear()
-        return r.json()
+    def update(self):
+        new_items = self.pocket.retrieve()
+        if new_items is not None:
+            self.items = new_items
 
-    def get_access_token_and_username(self) -> tuple[str, str]:
-        # if we already have it stored in file, load it and return it
-        if Pocket.POCKET_ACCESS_TOKEN_FILE.is_file():
-            with open(Pocket.POCKET_ACCESS_TOKEN_FILE, "r") as infile:
-                d = json.load(infile)
-                return d["access_token"], d["username"]
+    def sort_by_time_added(self, reverse=False) -> None:
+        idx = 1
+        for item in sorted(self.items.values(), key=lambda item: item.time_added, reverse=reverse):
+            item.sort_idx = idx
+            idx += 1
 
-        # else open a browser and proceed with the authorization process
-        request_token = self.get_request_token()
-        self.redirect_to_authorization(request_token)
-        d = self.request_access_token(request_token)
-        with open(Pocket.POCKET_ACCESS_TOKEN_FILE, "w") as outfile:
-            json.dump(d, outfile)
-        return d["access_token"], d["username"]
+    def sort_by_time_to_read(self, reverse=False) -> None:
+        idx = 1
+        for item in sorted(self.items.values(), key=lambda item: item.time_to_read, reverse=reverse):
+            item.sort_idx = idx
+            idx += 1
 
-    def get_request_token(self):
-        data = {
-            "consumer_key": Pocket.CONSUMER_KEY,
-            "redirect_uri": Pocket.REDIRECT_URI,
-        }
-        return self.send_request("v3/oauth/request", data)["code"]
-
-    def redirect_to_authorization(self, request_token):
-        sp.run(
-            [
-                OPEN_COMMAND,
-                (
-                    "https://getpocket.com/auth/authorize"
-                    f"?request_token={request_token}"
-                    f"&redirect_uri={Pocket.REDIRECT_URI}"
-                ),
-            ]
-        )
-        input('Click "Authorize" on the window that opened, and then press Enter')
-
-    def request_access_token(self, request_token):
-        data = {
-            "consumer_key": Pocket.CONSUMER_KEY,
-            "code": request_token,
-        }
-        return self.send_request("v3/oauth/authorize", data)
-
-    def validate_and_get_data(
-        self,
-        state=None,
-        favorite=None,
-        tag=None,
-        content_type=None,
-        sort=None,
-        detail_type=None,
-        search=None,
-        domain=None,
-        since=None,
-        count=None,
-        offset=None,
-    ) -> dict[str, str]:
-        data = {"consumer_key": Pocket.CONSUMER_KEY, "access_token": self.access_token}
-        if state:
-            assert state in ("unread", "archive", "all")
-            data["state"] = state
-        if favorite:
-            assert favorite in ("0", "1")
-            data["favorite"] = favorite
-        if tag:
-            data["tag"] = tag
-        if content_type:
-            assert content_type in ("article", "video", "image")
-            data["contentType"] = content_type
-        if sort:
-            assert state in ("newest", "oldest", "title", "site")
-            data["sort"] = sort
-        if detail_type:
-            assert detail_type in ("simple", "complete")
-            data["detailType"] = detail_type
-        if search:
-            data["search"] = search
-        if domain:
-            data["domain"] = domain
-        if since:
-            data["since"] = since
-        if count:
-            assert int(count) > 0
-            data["count"] = count
-        if offset:
-            assert int(offset) > 0
-            data["offset"] = offset
-        return data
-
-    def retrieve(
-        self,
-        state=None,
-        favorite=None,
-        tag=None,
-        content_type=None,
-        sort=None,
-        detail_type=None,
-        search=None,
-        domain=None,
-        since=None,
-        count=None,
-        offset=None,
-    ) -> dict[str, PocketItem]:
-        """
-        Return a dictionary with key the item_id (from unique ID comming from Pocket) and value a PocketItem
-        """
-        data = self.validate_and_get_data(
-            state, favorite, tag, content_type, sort, detail_type, search, domain, since, count, offset
-        )
-        r_json = self.send_request("v3/get", data)
-
-        if not r_json["list"]:
+    def display(self):
+        if not self.items or len(self.items) == 0:
+            print("\n\nNo items found\n\n")
             return
 
-        items = dict()
-        for item_id, data in r_json["list"].items():
-            items[item_id] = PocketItem(
-                item_id,
-                data.get("domain_metadata", {}).get("name", "N/A"),
-                data.get("given_title", "N/A"),
-                data.get("given_url", "N/A"),
-                data.get("time_added", "N/A"),
-                data.get("time_to_read", "0"),
-                data.get("word_count", "0"),
+        match self.sort_by:
+            case 1:
+                self.sort_by_time_added()
+            case 2:
+                self.sort_by_time_added(reverse=True)
+            case 3:
+                self.sort_by_time_to_read()
+            case 4:
+                self.sort_by_time_to_read(reverse=True)
+
+        headers = (
+            "Index",
+            "Domain Name",
+            "Title",
+            "URL",
+            "Added",
+            "Time to Read",
+            "Word Count",
+        )
+
+        items = [
+            (
+                item.sort_idx,
+                item.domain_name,
+                item.given_title,
+                item.given_url,
+                datetime.fromtimestamp(item.time_added),
+                item.time_to_read,
+                item.word_count,
+            )
+            for item in sorted(self.items.values(), key=lambda item: item.sort_idx, reverse=True)
+        ]
+
+        num_columns = len(headers)
+        max_len = [len(header) + 4 for header in headers]
+        max_len[0] -= 1
+        max_len[-1] -= 1
+        for item in items:
+            for i in (0, num_columns - 1):
+                length = len(str(item[i])) + 1
+                if max_len[i] < length:
+                    max_len[i] = length
+
+            for i in range(1, num_columns - 1):
+                length = len(str(item[i])) + 2
+                if max_len[i] < length:
+                    max_len[i] = length
+
+        term_cols, _ = shutil.get_terminal_size()
+        remaining = term_cols - sum(max_len[:2]) - sum(max_len[4:]) - 4
+        title_cols = min(max_len[2] - 2, remaining * 4 // 5)
+        url_cols = remaining - title_cols
+        max_len[2] = title_cols
+        max_len[3] = url_cols
+        for i in range(len(items)):
+            item = items[i]
+            items[i] = (
+                item[0],
+                item[1],
+                get_trimmed(item[2], title_cols),
+                get_trimmed(item[3], url_cols),
+                item[4],
+                item[5],
+                item[6],
             )
 
-        return items
+        to_print = tabulate(items, headers=headers)
+        lines = to_print.split("\n")
+        print(to_print)
+        print(lines[1])
+        print(lines[0])
+        print()
 
-    def request_delete(self, item_id):
-        data = {
-            "consumer_key": Pocket.CONSUMER_KEY,
-            "access_token": self.access_token,
-            "actions": [{"action": "delete", "item_id": item_id}],
-        }
-        self.send_request("v3/send", data)
+    def prompt_filter(self, match):
+        self.items = dict(
+            filter(
+                lambda pair: match in pair[1].given_url or match in pair[1].given_title,
+                self.items.items(),
+            )
+        )
 
-    def request_tags_add(self, item_id: str, tags: str):
-        """
-        Add one or more tags to an item.
+    def prompt_domains(self):
+        domains = defaultdict(int)
+        for item in self.items.values():
+            domain = urlparse(item.given_url).hostname
+            domains[domain] += 1
+        domains = list(domains.items())
 
-        item_id: The id of the item to perform the action on.
-        tags: A comma-delimited list of one or more tags.
-        """
+        while True:
+            for idx, (domain, count) in enumerate(domains, start=1):
+                print(f"{idx}. {count}: {domain}")
+            try:
+                domain_idx = int(input("> "))
+                if 1 <= domain_idx <= len(domains):
+                    filter_domain = domains[domain_idx - 1][0]
+                    break
+                raise ValueError
+            except ValueError:
+                print("Invalid index\n")
+                pass
 
-        self.batched_actions.append({"action": "tags_add", "item_id": item_id, "tags": tags})
+        self.items = dict(
+            filter(
+                lambda pair: urlparse(pair[1].given_url).hostname == filter_domain,
+                self.items.items(),
+            )
+        )
 
-    def request_tags_clear(self, item_id: str):
-        """
-        Remove all tags from an item.
+    def prompt_length(self):
+        # show possible groups of 5 minutes increment
+        groups: set[int] = set()
+        for item in self.items.values():
+            groups.add(item.time_to_read // 5)
 
-        item_id: The id of the item to perform the action on.
-        tags: A comma-delimited list of one or more tags.
-        """
+        groups = list(sorted(groups))
+        while True:
+            try:
+                for idx, group in enumerate(groups, 1):
+                    print(f"{idx}: {group * 5}–{(group + 1)*5} minutes")
+                group_idx = int(input("> "))
+                if 1 <= group_idx <= len(groups):
+                    break
+            except ValueError:
+                pass
 
-        self.batched_actions.append({"action": "tags_clear", "item_id": item_id})
+        min_time_to_read = groups[group_idx - 1] * 5
+        max_time_to_read = (groups[group_idx - 1] + 1) * 5
+
+        self.items = dict(
+            filter(
+                lambda pair: min_time_to_read <= pair[1].time_to_read < max_time_to_read,
+                self.items.items(),
+            )
+        )
+
+    def prompt_sort(self) -> None:
+        while True:
+            try:
+                print("1: Oldest to Newest")
+                print("2: Newest to Oldest")
+                print("3: Shortest to Longest")
+                print("4: Longest to Shortest")
+                idx = int(input("> "))
+                if 1 <= idx <= 4:
+                    break
+            except ValueError:
+                pass
+        self.sort_by = idx
+
+    def get_cmd_and_idx(self):
+        while True:
+            tokens = input("  |  ".join(PocketPrompt.valid_commands.values()) + " > ").split()
+
+            if not tokens:
+                continue
+            if tokens[0] not in PocketPrompt.valid_commands.keys():
+                continue
+            if tokens[0] in ("q", "u", "l", "s", "t") and len(tokens) != 1:
+                continue
+            if tokens[0] in ("v", "d", "vd", "f", "p") and len(tokens) != 2:
+                continue
+
+            return tokens
+
+    def prompt(self):
+        self.update()
+        self.display()
+
+        while True:
+            tokens = self.get_cmd_and_idx()
+            cmd = tokens[0]
+
+            match cmd:
+                case "q":
+                    return
+                case "u":
+                    self.update()
+                    self.display()
+                    continue
+                case "l":
+                    self.prompt_length()
+                    self.display()
+                    continue
+                case ".":
+                    self.prompt_domains()
+                    self.display()
+                    continue
+                case "f":
+                    match = tokens[1]
+                    self.update()
+                    if match:
+                        self.prompt_filter(match)
+                    self.display()
+                    continue
+                case "s":
+                    self.prompt_sort()
+                    self.display()
+                    continue
+                case "t":
+                    self.update_tags()
+                case "da":
+                    for item_id in self.items.keys():
+                        self.pocket.request_delete(item_id)
+                    self.update()
+                    self.display()
+                    continue
+                case _:
+                    idx = int(tokens[1])
+
+                    # search for the index
+                    item_id = None
+                    for k, v in self.items.items():
+                        if v.sort_idx == idx:
+                            item_id = k
+                            break
+
+                    if not item_id:
+                        continue
+
+                    match cmd:
+                        case "v":
+                            sp.Popen(
+                                [OPEN_COMMAND, self.items[item_id].given_url],
+                                stdout=sp.DEVNULL,
+                                stderr=sp.DEVNULL,
+                            )
+                        case "d":
+                            self.pocket.request_delete(item_id)
+                            del self.items[item_id]
+                            self.display()
+                        case "vd":
+                            sp.Popen(
+                                [OPEN_COMMAND, self.items[item_id].given_url],
+                                stdout=sp.DEVNULL,
+                                stderr=sp.DEVNULL,
+                            )
+                            self.pocket.request_delete(item_id)
+                            del self.items[item_id]
+                            self.display()
+                        case "p":
+                            formatted_date = datetime.fromtimestamp(self.items[item_id].time_added).strftime('%Y-%m-%d')
+                            savefilename=clean_pdf_filename(f"{formatted_date} {self.items[item_id].given_title}.pdf")
+                            sp.Popen(
+                                [PERCOL_COMMAND, "pdf", "--css", "a:after { display: none }", f"--output={SAVE_PDF_DIR}/{savefilename}", self.items[item_id].given_url],
+                                stdout=sp.DEVNULL,
+                                stderr=sp.DEVNULL,
+                            )
+                            #self.pocket.request_delete(item_id)
+                            #del self.items[item_id]
+                            self.display()
+    def update_tags(self):
+        for item_id in self.items.keys():
+            self.pocket.request_tags_clear(item_id)
+
+        for item_id, item in self.items.items():
+            self.pocket.request_tags_add(item_id, f"{item.get_grouped_time_to_read()}-min")
+
+        self.pocket.send_batched_requests()
